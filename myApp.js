f7e61a5aa9e58193feacdebe90374c0b532cb99b
arsh-nav <arshia.nawab@gmail.com>
2025-08-21 14:56:54 +0200

    Third_11

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 /** 1) Install & Set up mongoose */
 
-const mongoose = require('mongoose');
+
 mongoose.connect(process.env.MONGO_URI);
 
 /** 2) Create a 'Person' Model */
