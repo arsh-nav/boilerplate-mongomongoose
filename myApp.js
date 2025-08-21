1d548f455047749aaa6116a44187ef0d0ff11598
arsh-nav <arshia.nawab@gmail.com>
2025-08-21 14:45:57 +0200

    Third_8

--- myApp.js
+++ myApp.js
@@ -2,8 +2,6 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-// Connect to MongoDB Atlas
-const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
